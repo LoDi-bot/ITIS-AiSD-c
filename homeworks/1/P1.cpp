d075d10c12ac415b9a962779ed2c7e4dba95b669
LoDi <asadullin.iskander@gmail.com>
2021-03-18 16:28:05 +0300

    answer P1

--- homeworks/1/P1.cpp
+++ homeworks/1/P1.cpp
@@ -7,34 +7,43 @@ struct element {
     element* next;
 };
 
-element* add(element* head, int x) {
+struct list {
+    element* head;
+    element* last;
+};
+
+list add_last(list l, int x) {
     element* temp = new element;
-    temp->value =  x;
-    temp->next = head;
-    return temp;
+    temp->value = x;
+    temp->next = nullptr;
+    if (l.last) {
+        l.last->next = temp;
+    }
+    if (!l.head) {
+        l.head = temp;
+    }
+    l.last = temp;
+    return l;
 }
 
-element* emptyList() {
-    return NULL;
-}
-
-void printList(element* head) {
-    element* temp = head;
+void printList(list l) {
+    element* temp = l.head;
     while (temp) {
         cout << temp->value << " ";
         temp = temp->next;
     }
-    cout << endl;
 }
 
 int main() {
     int n, counter = 0, i = 2;
     cin >> n;
-    element* head = emptyList();
+    list l;
+    l.last = nullptr;
+    l.head = nullptr;
     while (counter < n) {
         bool prime = true;
         if (i != 2) {
-            element* list = head;
+            element* list = l.head;
             while (list) {
                 if (i % list->value == 0) {
                     prime  = false;
@@ -44,11 +53,11 @@ int main() {
             }
         }
         if (prime) {
-            head = add(head, i);
+            l = add_last(l, i);
             counter++;
         }
         i++;
     }
-    printList(head);
+    printList(l);
     return 0;
 }
